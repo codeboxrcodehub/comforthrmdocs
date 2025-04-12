27a2e1482fe0a810fb3e17bca4233d67f82b8573
Sabuj Kundu <sabuj@codeboxr.com>
2025-04-13 01:22:29 +0600

    icons and url updated

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -33,8 +33,8 @@ export default {
 
     return head
   },*/
-  title: "Comfort HRM",
-  description: "An awesome docs template built by me",
+  title: "Comfort HRM Documentation",
+  description: "Documentation for Comfort HRM plugins",
   lang: 'en-US',
   // cleanUrls: true,
   //base: '/comforthrmdocs/',
@@ -57,13 +57,13 @@ export default {
     //Social Icons
     socialLinks: [
       { icon: "github", link: "https://github.com/codeboxrcodehub/comforthrmdocs" },
-      { icon: "twitter", link: "https://twitter.com/victorekea" },
+      //{ icon: "twitter", link: "https://twitter.com/victorekea" },
       //{ icon: "discord", link: "..." },
       {
         icon: {
-          svg: '<svg role="img" width="26.01" height="32" viewBox="0 0 256 315"><path d="M213.803 167.03c.442 47.58 41.74 63.413 42.197 63.615c-.35 1.116-6.599 22.563-21.757 44.716c-13.104 19.153-26.705 38.235-48.13 38.63c-21.05.388-27.82-12.483-51.888-12.483c-24.061 0-31.582 12.088-51.51 12.871c-20.68.783-36.428-20.71-49.64-39.793c-27-39.033-47.633-110.3-19.928-158.406c13.763-23.89 38.36-39.017 65.056-39.405c20.307-.387 39.475 13.662 51.889 13.662c12.406 0 35.699-16.895 60.186-14.414c10.25.427 39.026 4.14 57.503 31.186c-1.49.923-34.335 20.044-33.978 59.822M174.24 50.199c10.98-13.29 18.369-31.79 16.353-50.199c-15.826.636-34.962 10.546-46.314 23.828c-10.173 11.763-19.082 30.589-16.678 48.633c17.64 1.365 35.66-8.964 46.64-22.262"/></svg>',
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><g fill="none" fill-rule="evenodd"><path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8"/></g></svg>',
         },
-        link: "https://google.com",
+        link: "https://comforthrm.com",
       },
     ],
 
